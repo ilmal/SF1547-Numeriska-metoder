3ef9f863ae2bb32138a33a37e592611249664510
glass123banan <glass123banan@gmail.com>
2025-02-18 12:06:16 +0100

    uppg2 klar

--- labb2/Vattentorn.m
+++ labb2/Vattentorn.m
@@ -1,7 +1,9 @@
 % Uppgift 1a
 % -----------------------------
 % Fördefinierade värden & funktion
-beta = 0.2;
+% 0.1 ≤ beta ≤ 0.3
+% 0 ≤ x ≤ 20
+beta = 0.2; 
 y = @(x) (exp(beta*x)+8)/(1+(x/5)^3);
 
 % Trapetsmetoden -- uersekta vart är punkterna????
