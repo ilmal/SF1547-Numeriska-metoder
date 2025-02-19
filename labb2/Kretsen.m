d85aa84a05f26df58d43000edc2216456e059d9d
glass123banan <glass123banan@gmail.com>
2025-02-20 00:01:33 +0100

    börjat på 3

---
+++ labb2/Kretsen.m
@@ -0,0 +1,35 @@
+format long;
+
+% Uppgift 3a
+% -----------------------------
+% Spänningen i kretsen ges av: 
+% Lq''(t) + Rq'(t) + (1/C)q(t) = 0 
+% q(0) = 1 
+% dq/dt (0) = 0
+
+% Strömmen ges av: 
+% i = dq/dt
+
+% Andraderivatan skrivs om till: q'' = -((1/C)q - Rq')/L
+
+% Omskrivning:
+% q'' = f(t, q, q') 
+% -> Q(t) = q(t), I(t) = q'(t)
+% -> Q' = q' = I, I' = q'' = f(t, q, q') = -((1/C)Q - RI)/L
+
+U = [Q; I];
+F = [   I; 
+    -((1/C)*Q + R*I)/L]; % F(t, U)
+
+U0 = [1; 0]; % U(t0) = [c1, c2]
+
+% -----------------------------
+% Uppgift 3b
+
+function [F] = returnVectorF(t, y, R, L, C)
+    % TODO: beräkna vektorn F
+end
+
+
+
+
---
+++ labb2/VattentornSekant.m
@@ -0,0 +1,8 @@
+format long;
+
+% Uppgift 1b
+% -----------------------------
+% Hitta ett beta så att volymen blir 1500m^3
+
+% F(beta) = pi * integral(0, 20, y(x; beta)^2)dx - 1500 = 0
+
