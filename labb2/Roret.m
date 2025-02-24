b9482a91d6c45b1128b42cfbbbceaf6c63dc83ce
glass123banan <glass123banan@gmail.com>
2025-02-24 14:36:51 +0100

    KLARRRRRRRgit add .git add .git add .git add .git add .git add . (hoppas jag)

--- labb2/Roret.m
+++ labb2/Roret.m
@@ -5,26 +5,31 @@ format long;
 
 % givna värden
 a = 1; k = 1; Ti = 450; Te = 20; r0 = 1; rslut = 2; 
-N = 25; % N+1 delintervall
+N = 25; % ska vara N+1 delintervall
 
-tol = 10^(-2);
-diff = 0.1;
-prev_T_end = 0;
+tol = 10^(-2.5); % tolerans för att garantera första decimalen korrekt
+diff = 0.1;    % godtyckligt startvärde på diff
+prev_T_end = 0; % godtyckligt startvärde på tidigare T(end)
 
+
+fprintf('----------------------------------------------------------\n')
+fprintf('Uppgift 4c: Tend där N fördubblas varje iteration\n \n')
+
+% while-loop tills given tolerans är uppnådd
 while diff > tol
     h = (rslut-r0)/(N+1); % steglängd
-
-    A = zeros(N, N); % skapa default 0-matris med rätt dimensioner
+    
+    % Ax=b behandlar endast inre punkter och ej randpunkter
+    A = zeros(N, N); % skapa default 0-matris med dimensioner NxN
     b = zeros(N, 1); % skapa vektorn b i ekvationen Ax = b
     
-    % ri = (r0+h:h:rslut-h)'; % alla punkter r med steglängd h
-    ri = linspace(r0 + h, rslut - h, N)';
+    ri = (r0+h:h:rslut-h)'; % alla inre punkter r med steglängd h
     
     % sätt kolumn 1-2 i rad 1 (rad 1 behandlas separat)
-    A(1, 1:2) = [-2*ri(1)/h^2, (ri(1)/h^2)+(1/(2*h))];
-    b(1) = -Ti*((ri(1)/h^2)-(1/(2*h)));
+    A(1, 1:2) = [-2*ri(1)/h^2, (ri(1)/h^2)+(1/(2*h))]; % beräknade värden
+    b(1) = -Ti*((ri(1)/h^2)-(1/(2*h))); % beräknade värden
 
-    % for-loop genom inre rader
+    % for-loop genom inre rader med ihopsamlade termer
     for ii=2:N-1
         % sätt rad ii och kolumn ii-1
         A(ii,ii-1) = (ri(ii)/h^2)-(1/(2*h));
@@ -35,21 +40,22 @@ while diff > tol
         % sätt rad ii och kolumn ii+1
         A(ii, ii+1) = (ri(ii)/h^2)+(1/(2*h));
     end
-
-    % sista raden N, sätt sista två kolumnerna N-1 och N
+    
+    % sista raden N behandlas separat, sätt sista två kolumnerna N-1 och N
     A(N, N-1:N) = [(ri(end)/h^2)-(1/(2*h)), (-2*ri(end)/h^2)+(k/(k+a*h))*((ri(end)/h^2)+(1/(2*h)))];
     b(N) = -a*h*Te/(k+a*h)*((ri(end)/h^2)+(1/(2*h)));
     
     A = sparse(A); % lagra som gles matris
     
-    T = A\b;
+    T = A\b; % lös systemet och spara i T
 
-    Tend = (a*h*Te+k*T(end))/(k+a*h);
+    Tend = (a*h*Te+k*T(end))/(k+a*h); % beräkna T(N+1), randpunkten
     
     fprintf('Iteration with N = %d: T(N+1) = %.10f\n', N, Tend);
 
-    diff = abs(T(end)-prev_T_end);
-    prev_T_end = T(end);
+    % beräkna diff absolutbelopp på sista punkten Tend
+    diff = abs(Tend-prev_T_end);
+    prev_T_end = Tend; % uppdatera senaste Tend
 
     fprintf('Difference at the moment: %.10f\n \n', diff);
         
@@ -57,8 +63,58 @@ while diff > tol
     N = 2 * N;
 end
 
-fprintf('\nFinal T(end) = %.10f\n', T(end));
+fprintf('Final T(end) = %.10f\n', Tend);
 
+% --------------------------------------------------
+% Uppgift 4d
 
+k = 1; N_a = 20; % värde på k samt antal delintervall som N_a
+a0 = 0; aslut = 10; % alpha intervall
+N = 10000; % värde på N för beräkning
 
+h_a = (aslut-a0)/N_a; % steglängd för alpha
 
+ai = a0:h_a:aslut; % skapa alla intervaller för alpha
+
+fprintf('----------------------------------------------------------\n')
+fprintf('Uppgift 4d: Tend baserat på alpha = [0, 10]\n \n')
+
+% for-loop genom alla värden på alpha med steglängd h_a
+for jj = 1:length(ai)
+    h = (rslut-r0)/(N+1); % steglängd för N
+    
+    % Ax=b behandlar endast inre punkter och ej randpunkter
+    A = zeros(N, N); % skapa default 0-matris med dimensioner NxN
+    b = zeros(N, 1); % skapa vektorn b i ekvationen Ax = b
+    
+    ri = (r0+h:h:rslut-h)'; % alla inre punkter r med steglängd h
+    
+    % sätt kolumn 1-2 i rad 1 (rad 1 behandlas separat)
+    A(1, 1:2) = [-2*ri(1)/h^2, (ri(1)/h^2)+(1/(2*h))]; % beräknade värden
+    b(1) = -Ti*((ri(1)/h^2)-(1/(2*h))); % beräknade värden
+
+    % for-loop genom inre rader med ihopsamlade termer
+    for ii=2:N-1
+        % sätt rad ii och kolumn ii-1
+        A(ii,ii-1) = (ri(ii)/h^2)-(1/(2*h));
+        
+        % sätt rad ii och kolumn ii
+        A(ii, ii) = -2*ri(ii)/h^2;
+    
+        % sätt rad ii och kolumn ii+1
+        A(ii, ii+1) = (ri(ii)/h^2)+(1/(2*h));
+    end
+    
+    % sista raden N behandlas separat, sätt sista två kolumnerna N-1 och N
+    A(N, N-1:N) = [(ri(end)/h^2)-(1/(2*h)), (-2*ri(end)/h^2)+(k/(k+ai(jj)*h))*((ri(end)/h^2)+(1/(2*h)))];
+    b(N) = -ai(jj)*h*Te/(k+ai(jj)*h)*((ri(end)/h^2)+(1/(2*h)));
+    
+    A = sparse(A); % lagra som gles matris
+    
+    T = A\b; % lös systemet och spara i T
+
+    Tend = (a*h*Te+k*T(end))/(k+ai(jj)*h); % beräkna T(N+1), randpunkten
+    
+    % printa resultatet på Tend för nuvarande alpha-värde
+    fprintf('Iteration med a = %.1f: T(N+1) = %.6f\n', ai(jj), Tend);
+end
